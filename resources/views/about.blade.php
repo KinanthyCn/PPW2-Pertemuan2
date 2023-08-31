c350f9677c0bd8013d950077e884d12c552310d7
KinanthyCn <kinanthycahyaningrum@mail.ugm.ac.id>
2023-08-31 14:30:12 +0700

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    {{ $saya }}
+
+    <!-- nambahin script js -->
+    <script src="js/script.js"></script>
+    
+</body>
+</html>
